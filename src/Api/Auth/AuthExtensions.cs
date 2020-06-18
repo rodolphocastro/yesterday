161320f455478f3cb0b7841b0e6d9eea8f20ae8e
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-17 21:42:15 -0300

    feat auth: Add Auth services and middleware

---
+++ src/Api/Auth/AuthExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Auth
+{
+    /// <summary>
+    /// Extensions for adding and setting up Authentication and Authorization on the API.
+    /// </summary>
+    public static class AuthExtensions
+    {
+        /// <summary>
+        /// Adds services to handle Authentication and Authorization.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="tkAction"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAuth(this IServiceCollection services, Action<TokenSettings> tkAction = null)
+        {
+            if (tkAction is null)
+            {
+                tkAction = (_) => { };
+            }
+
+            var tokenSettings = new TokenSettings();
+            tkAction(tokenSettings);
+
+            return services.AddAuth(tokenSettings);
+        }
+
+        private static IServiceCollection AddAuth(this IServiceCollection services, TokenSettings settings)
+        {
+            services
+                .AddAuthentication(s =>
+                {
+                    s.DefaultAuthenticateScheme = s.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(s =>
+                {
+                    s.SaveToken = true;
+                    s.Authority = settings.Authority;
+                    s.Audience = settings.Audience;
+                    s.MetadataAddress = string.IsNullOrWhiteSpace(settings.WellKnownAddress) ? null : settings.WellKnownAddress;
+                });
+            return services;
+        }
+    }
+}
--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Api.Auth;
 using Api.OpenApi;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Api
 {
@@ -26,6 +21,7 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuth(c => Configuration.Bind(TokenSettings.TokenSettingsKey, c));
             services.AddControllers();
             services.AddOpenApiGen();
         }
@@ -42,6 +38,7 @@ namespace Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseOpenApi();
             app.UseEndpoints(endpoints =>
