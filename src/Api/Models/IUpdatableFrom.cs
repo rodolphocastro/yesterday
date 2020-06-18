d644393887e0b1a62b6ffb25925212054114932e
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-18 07:51:13 -0300

    feat profile: Add IUpdatebleFrom
    This interface will aid us on creating a pattern for what properties are updatable on an object

---
+++ src/Api/Models/IUpdatableFrom.cs
@@ -0,0 +1,28 @@
+﻿namespace Api.Models
+{
+    /// <summary>
+    /// Describes methods to update an instance of T by using properties of an instance of G.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="G"></typeparam>
+    public interface IUpdatableFrom<out T, in G>
+        where T : class
+        where G : class
+    {
+        /// <summary>
+        /// Apply the desired changes to the current object.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <returns></returns>
+        T ApplyChanges(G from);
+    }
+
+    /// <summary>
+    /// Describes methdos to update and instance of T by using another instance of T.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IUpdatableFrom<T> : IUpdatableFrom<T, T>
+        where T : class
+    {
+    }
+}
