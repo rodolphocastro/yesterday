a6d9e89b4a3ac47cfd285f836183fbe16fa31b24
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-17 22:19:41 -0300

    feat auth: Add Supported Scopes

---
+++ src/Api/Auth/Scopes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Api.Auth
+{
+    /// <summary>
+    /// Scopes supported by the API.
+    /// </summary>
+    internal static class Scopes
+    {
+        internal static class Manage
+        {
+            public const string Profile = "manage:profile";
+            public const string Projects = "manage:projects";
+        }
+
+        internal static IDictionary<string, string> AllScopes { get; } = new Dictionary<string, string>
+        {
+            { Manage.Profile, "Allows the application to manage your Yesterday Profile" },
+            { Manage.Projects, "Allows the application to manage your Projects" }
+        };
+    }
+}
